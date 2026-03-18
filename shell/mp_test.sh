7847fd568fefa802b3a3a73f7492392f6fc83eff
yanshikun2963 <2731794405@qq.com>
2026-03-18 18:17:01 +0800

    initial commit

---
+++ shell/mp_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES="0"
+export num_gpu=1
+export use_multi_gpu=false
+export task='predcls'
+
+# 定义权重文件名列表
+weight_files=(  
+                # "model_0005000.pth" 
+                # "model_0030000.pth"
+                # "model_0035000.pth"
+                # "model_0040000.pth"
+                # "model_0045000.pth" 
+                "model_0050000.pth"
+                "model_0055000.pth"
+                # "model_0015000.pth"
+                # "model_0020000.pth"
+                )
+
+export save_result=False
+export output_dir="/home/yj/zgw/het/ckpt/predcls-mope_lxmert2-vg"  # 检查点目录
+
+# 遍历权重文件列表
+for weight_file in "${weight_files[@]}"
+do
+    echo "Running with weight file: ${weight_file}"
+    
+    python tools/relation_test_net.py --config-file "${output_dir}/config.yml" \
+            TEST.IMS_PER_BATCH 8 \
+            TEST.SAVE_RESULT ${save_result} \
+            OUTPUT_DIR ${output_dir} \
+            MODEL.WEIGHT "${output_dir}/${weight_file}"
+done
